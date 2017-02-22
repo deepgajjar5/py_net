49d2601de2320004e3c78b83853dfd628216bb6f
Deep Gajjar <deep.gajjar@colorado.edu>
2017-02-22 09:34:00 -0800

    Week7 changes

--- week7/output.py
+++ week7/output.py
@@ -2,21 +2,28 @@ from jsonrpclib import Server
 import sys
 import ssl
 import re
+import pyeapi
 ssl._create_default_https_context = ssl._create_unverified_context
 
 username = "eapi"
 password = "17mendel"
 host = "184.105.247.75"
 
+list_of_vlans = []
+
+#conn = pyeapi.connect_to("pynet-sw4")
+
+#vlans = conn.api("vlans")
+
+#all_vlans = vlans.getall()
+
+#for key, value in all_vlans.iteritems():
+    #print key
+
 conn = Server("https://%s:%s@%s/command-api" %(username, password, host))
 
-list_cmd = [ "enable", "configure", "show interfaces" ]
+list_cmd = [ "enable", "configure", "show vlan" ]
 
 output = conn.runCmds(1, list_cmd, "text")
 
-num_output = str(output[2]['output'])
-
-#list_of_interfaces = re.findall('.* is .*[up|down], .*', num_output)
-
-print num_output
-
+print output[2]['output'].split('\n')[2:]
--- week7/test.py
+++ week7/test.py
@@ -2,8 +2,33 @@ from jsonrpclib import Server
 import sys
 import ssl
 import re
+import pyeapi
+ssl._create_default_https_context = ssl._create_unverified_context
 
-with open("file1.txt", "r+") as f:
-    output = f.read()
+username = "eapi"
+password = "17mendel"
+host = "184.105.247.75"
 
-print re.findall('.* is up.*', output)
+list_of_vlans = []
+
+#conn = pyeapi.connect_to("pynet-sw4")
+
+#vlans = conn.api("vlans")
+
+#all_vlans = vlans.getall()
+
+#for key, value in all_vlans.iteritems():
+    #print key
+
+conn = Server("https://%s:%s@%s/command-api" %(username, password, host))
+
+list_cmd = [ "enable", "configure", "show vlan" ]
+
+output = conn.runCmds(1, list_cmd, "text")
+
+for i in output[2]['output'].split('\n')[2:]:
+    if i != None:
+        list_of_vlans.append(i[0:3])
+
+print ' '.join(list_of_vlans).split()
+
